6e970f662c15e38161cff69c3b009ec6666a0235
Petr Nemec <petr.nemec@tieto.com>
2017-10-23 22:04:44 +0200

    wip

---
+++ vsphere/dvportgroup_list.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from vsphere import vSphere
+from vsphere import credentials
+from vsphere import seldc
+import sys
+
+# Select the vSphere to be modified
+inputs = 'inputs/vsphere_' + seldc(sys.argv[1:]) + '.yml'
+
+
+# Create an instance of Class vSphere
+cred = credentials(inputs)
+vmw = vSphere(*cred)
+
+# List all portgroups.
+vmw.list_portgroups()
