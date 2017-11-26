49cfd6fd039d7c4df819f853e1403ace2e490b6e
Petr Nemec <petr.nemec@tieto.com>
2017-11-26 21:58:09 +0100

    vm to template

---
+++ add_serial/vm_to_template.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+from pyVmomi import vim
+from pyVim.connect import SmartConnect, Disconnect
+import atexit
+from tools import tasks
+import argparse
+import getpass
+import ssl
+
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+context.verify_mode = ssl.CERT_NONE
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Arguments for talking to vCenter')
+
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSphere service to connect to')
+
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use')
+
+    parser.add_argument('-v', '--vm-name',
+                        required=False,
+                        action='store',
+                        help='name of the vm')
+
+    parser.add_argument('--uuid',
+                        required=False,
+                        action='store',
+                        help='vmuuid of vm')
+
+
+
+    args = parser.parse_args()
+
+    if not args.password:
+        args.password = getpass.getpass(
+            prompt='Enter password')
+
+    return args
+
+
+def get_obj(content, vimtype, name):
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True)
+    for c in container.view:
+        if c.name == name:
+            obj = c
+            break
+    return obj
+
+
+
+def main():
+    args = get_args()
+
+    # connect this thing
+    si = SmartConnect(
+        host=args.host,
+        user=args.user,
+        pwd=args.password,
+        port=args.port)
+    # disconnect this thing
+    atexit.register(Disconnect, si)
+
+    vm = None
+    if args.uuid:
+        search_index = si.content.searchIndex
+        vm = search_index.FindByUuid(None, args.uuid, True)
+    elif args.vm_name:
+        content = si.RetrieveContent()
+        vm = get_obj(content, [vim.VirtualMachine], args.vm_name)
+
+    if vm:
+        vm.MarkAsTemplate()
+    else:
+        print "VM not found"
+
+    """
+    task = vm.MarkAsTemplate()
+    tasks.wait_for_tasks(si, [task])
+    """
+
+# start this thing
+if __name__ == "__main__":
+    main()
